43166546bf00124f984093b4e72f89ab153e2fe6
venkateshvc225 <venkateshvc225@gmail.com>
2019-01-29 00:14:26 +0530

    Validation and win and lose case impl

---
+++ app.js
@@ -0,0 +1,63 @@
+/*
+- GAME FUNCTION
+- Player must guess a number between min to max 
+- Player gets certain number of guesses 
+- Notify a player with remaining guesses
+- Notify  the  player with correct answer player fail to guess
+- let player choose play again
+*/
+
+//Game values
+let min = 1,
+    max = 10,
+    winnigNum = 2,
+    guessLeft = 3;
+
+//UI Elements
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-value'),
+    guessInput = document.querySelector('.guess-input'),
+    message = document.querySelector('.message');
+
+//Assign min max values
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//listen for event listener
+guessBtn.addEventListener('click',function(){
+    let guess = parseInt(guessInput.value);
+    //validate input
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter number between ${min} and ${max}`,'red');
+    }
+    //check if won
+    if(guess === winnigNum){
+        gameOver(true,`${winnigNum} is correct You Won!!`);
+
+    }else{
+        
+        if(guessLeft === 0){
+            gameOver(false,`Game over,you lost it!! The correct number is ${winnigNum}`);
+        }else {
+            guessLeft -= 1;
+            guessInput.style.borderColor = 'red';
+            setMessage(`Wrong guess, you left left ${guessLeft} guesses`,'red');
+            guessInput.value = '';
+        }
+    }
+});
+
+function gameOver(won,msg){
+    won === true ? color ='green':color = 'red';
+    guessInput.disable = true;
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    setMessage(msg);
+}
+// set message
+function setMessage(msg,color){
+    message.style.color = color;
+    message.textContent = msg;
+}
